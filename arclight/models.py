0217a15eb60ff85dd17efe57905b37d239eeefdd
Carlos Morán <carlos@alfredchat.com>
2018-10-28 17:03:24 -0600

    first commit

---
+++ arclight/models.py
@@ -0,0 +1,25 @@
+from djongo import models
+
+class NLUModel(models.Model):
+
+    _id = models.ObjectIdField()
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=512, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Intent(models.Model):
+    
+    _id = models.ObjectIdField()
+    nlu_model = models.ForeignKey(
+        NLUModel,
+        on_delete=models.CASCADE,
+        related_name = 'intents',
+    )
+    name = models.CharField(max_length=128, blank=False)
+    phrases = models.ListField(models.CharField(max_length=256))
+    answers = models.ListField(models.CharField(max_length=256))
+
+    def __str__(self):
+        return self.name
---
+++ arclight/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
+
+app_name = 'arclight'
+
+urlpatterns = [
+    
+]
